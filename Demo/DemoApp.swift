156ca0a202868e441bf7fcad6ae705fe76a25ac4
JunichiroM <sbxn4mxt@yahoo.co.jp>
2025-02-10 10:41:31 +0900

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by apple on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
